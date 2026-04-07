790c91c04bd9e4f7a9225cb1184bc5959b1b9977
zhaozino <zhaozino@163.com>
2026-04-08 01:32:28 +0800

    fdsa

--- frontend/api/file.js
+++ frontend/api/file.js
@@ -10,20 +10,43 @@ var fileApi = {
   uploadWithPresignedUrl: function (presignedResp, filePath) {
     return new Promise(function (resolve, reject) {
       var fileUrl = presignedResp.fileUrl
+      console.log('[COS上传] 开始上传')
+      console.log('[COS上传] uploadUrl:', presignedResp.uploadUrl)
+      console.log('[COS上传] fileUrl:', fileUrl)
+      console.log('[COS上传] filePath:', filePath)
 
-      wx.uploadFile({
-        url: presignedResp.uploadUrl,
+      // 先获取文件内容，再用 wx.request PUT 上传
+      wx.getFileSystemManager().readFile({
         filePath: filePath,
-        name: 'file',
-        method: 'PUT',
-        success: function (res) {
-          if (res.statusCode === 200 || res.statusCode === 204) {
-            resolve(fileUrl)
-          } else {
-            reject(new Error('上传失败：' + res.statusCode))
-          }
+        success: function (fileRes) {
+          console.log('[COS上传] 读取文件成功, 大小:', fileRes.data.byteLength)
+
+          wx.request({
+            url: presignedResp.uploadUrl,
+            method: 'PUT',
+            data: fileRes.data,
+            header: {
+              'Content-Type': 'image/png'
+            },
+            success: function (res) {
+              console.log('[COS上传] 响应状态码:', res.statusCode)
+              console.log('[COS上传] 响应数据:', res.data)
+              if (res.statusCode === 200 || res.statusCode === 204) {
+                resolve(fileUrl)
+              } else {
+                reject(new Error('上传失败：' + res.statusCode + ', ' + JSON.stringify(res.data)))
+              }
+            },
+            fail: function (err) {
+              console.error('[COS上传] 请求失败:', err)
+              reject(err)
+            }
+          })
         },
-        fail: reject
+        fail: function (err) {
+          console.error('[COS上传] 读取文件失败:', err)
+          reject(err)
+        }
       })
     })
   },
